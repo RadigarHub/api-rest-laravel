1af46f5bf5564cbbcd62a37838bca3d881cd1889
RadigarHub <diaz.garcia.rafa@gmail.com>
2020-07-08 15:23:09 +0200

    Clase 15

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
+    return '<h1>Hola mundo con Laravel</h1>';
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/pruebas/{nombre?}', function ($nombre = null) {
+    $texto = '<h2>Texto desde una ruta</h2>';
+    $texto .= 'Nombre: '.$nombre;
+    
+    return view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+
+Route::get('/animales', 'PruebasController@index');
+
+Route::get('/test-orm', 'PruebasController@testOrm');
